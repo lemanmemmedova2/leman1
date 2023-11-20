75e57ddd20ad22f4063343e8af5ce7bc763ca8a9
lemanmemmedova2 <memmedovaleman212@gmail.com>
2023-11-20 17:36:41 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Myprofile from './Component/Myprofile';
+
+function App() {
+  return (
+    <div>
+       <Myprofile/>
+        </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Myemail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Myemail = () => {
+  return (
+    
+        <p>Myemail: memmedovaleman212@gmail.com</p>
+  )
+}
+
+export default Myemail
